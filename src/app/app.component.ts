83368766c23ce60795c8da628f8a5f41463cee84
Satyakrishnajasti <satyajasti9@gmail.com>
2021-03-25 12:13:39 +0530

    latet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormArray,FormGroup,FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forms-test';
+  sample!:FormGroup;
+
+constructor(public fb:FormBuilder)
+{
+  this.sample=this.fb.group({
+    add:this.fb.array([
+    ])
+  });
+
+}
+
+ngOnInit():any{
+  
+}
+
+sample1():FormArray
+{
+  return this.sample.get('add') as FormArray
+}
+
+
+fields():FormGroup{
+  return this.fb.group({
+    name:[''],
+    place:[''],
+    email:['']
+  });
+}
+
+
+add()
+{
+  this.sample1().push(this.fields());
+}
+
+delete(index:number)
+{
+  this.sample1().removeAt(index);
+
+}
+
+display=():any =>{
+  console.log(this.sample.value);
+}
+
+
+
+
+
 }
